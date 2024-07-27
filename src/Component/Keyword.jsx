12b5dfee2189602b5f30efed50b8cd8101e35974
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2024-07-28 01:38:00 +0530

    Add key points in home page.

--- src/Component/Keyword.jsx
+++ src/Component/Keyword.jsx
@@ -2,23 +2,29 @@ import React from 'react';
 import Keywordbg from '../assets/image/Keywordbg2.jpg';
 import { GoDotFill } from "react-icons/go";
 
-const points = ["Provides best door step car washing and detailing services in Delhi/NCR Region.",
+const keyPoints = [
+  "Provides best door step car washing and detailing services in Delhi/NCR Region.",
   "Provides Best Car Detailing Services in Delhi/NCR Region.", 
   "Provides Best Car Rental Services at very affordable prices in Delhi/NCR Region.",
   "Best Place To Sell And Purchase Used Cars. (Services Upcoming)."
 ];
 
-function Keyword()  {
+function Keyword() {
   return (
-    <section style={{ backgroundImage: `url(${Keywordbg})` }} className="relative bg-cover bg-center text-slate-100 m-4 h-fit p-6">
-      <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
-      <div className="relative z-10">
-      {GoDotFill}
-              </div>
-              <span>{points}</span>
+    <section style={{ backgroundImage: `url(${Keywordbg})` }} className="relative bg-cover bg-center text-slate-100 ml-2 h-fit ">
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="relative z-10 backdrop-blur-sm">
+      <h1 className='text-sm m-4 font-bold p'>Key Points</h1>
+        {keyPoints.map((point, index) => (
+          
+          <div key={index} className="flex items-center pb-4">
+            <GoDotFill className="mr-2" />
+            <span>{point}</span>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
 
 export default Keyword;
-
